d3f4ba2e477604afe12c897cb7f57abef9516174
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2022-11-30 19:48:00 +0530

    Solve exercise 01 to exercise 07

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let strLength = string.length;
+    let returnStr = '';
 
+    for (let i = strLength-1; i >= 0; i--) {
+        returnStr += string[i];
+    }
+
+    return returnStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let arrLen = array.length;
+    let parameterLen = arguments.length;
+    let returnArr = [];
 
+    loop2 : for (let i = 0; i < arrLen; i++) {
+        for (let j = 1; j < parameterLen; j++) {
+            if (array[i] === arguments[j]) {
+                continue loop2;
+            }
+        }
+        returnArr.push(array[i]);
+    }
+
+    return returnArr;
 };
 
 // Do not edit below this line
