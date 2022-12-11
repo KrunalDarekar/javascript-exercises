66e5aded5c625bee2f96d0a9ed6f9f9f87c1c884
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2022-12-11 21:22:41 +0530

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+ const newString = string.toUpperCase().replace(/[^A-Z]/g, "");
+ for(let i = 0; i < newString.length/2; i++) {
+    if(newString[i] === newString[newString.length - 1 - i]){
+        continue;
+    } else {
+        return false;
+    }
+ }
+ return true;
 };
 
 // Do not edit below this line
