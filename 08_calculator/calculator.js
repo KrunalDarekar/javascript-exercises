5dada3ed638bf6ae18acd30a8808981fc3cfcd05
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2022-12-11 19:38:36 +0530

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(let i = 0; i < array.length; i++) {
+   sum += array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for(let i = 0; i < array.length; i++) {
+   product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let ans = 1;
+  for(let i = 0; i < b; i++) {
+    ans *= a;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for(let i = 2; i <= a; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
