cb84922c1cf2c98e36bdc9584dd746f3e9a3e52f
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2022-12-12 13:51:08 +0530

    Solve exercise 10 to 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(n < 0) {
+        return "OOPS";
+    }
 
+    let first = 1;
+    let second = 1;
+    let next = first + second;
+    for(let i = 0; i < n-1; i++) {
+        first = second;
+        second = next;
+        next = first + second;
+    }
+    return first;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let sorted = people.sort((a, b) => {
+        if(!a.yearOfDeath) {
+            aFactor = (new Date()).getFullYear();
+        } else {
+            aFactor = a.yearOfDeath;
+        }
 
+        if(!b.yearOfDeath) {
+            bFactor = (new Date()).getFullYear();
+        } else {
+            bFactor = b.yearOfDeath;
+        }
+
+        let aAge = aFactor - a.yearOfBirth;
+        let bAge = bFactor - b.yearOfBirth;
+
+        if(aAge > bAge){
+            return -1;
+        } else {
+            return 1;
+        }
+    })
+
+    return sorted[0];
 };
 
 // Do not edit below this line
